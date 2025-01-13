815120d05aab3154db71e81e0deffe7168bbd901
AmirBiG <itsamirbig@gmail.com>
2025-01-13 15:30:31 +0330

    all fix

--- app.js
+++ app.js
@@ -5,10 +5,6 @@ const pElem = $.querySelector(".p1");
 
 const urlGentelman = "./images/gentleman.jpg";
 
-// console.log(imageElem);
-// console.log(pElem);
-
-
 imageElem.addEventListener("click", () => {
     pElem.innerHTML = "Hello World";
     imageElem.src = urlGentelman;
--- resetBtn.js
+++ resetBtn.js
@@ -2,6 +2,7 @@ let btnElem = $.querySelector(".res");
 let imgElem = $.querySelector(".i");
 let p2Elem = $.querySelector(".p1");
 
+
 btnElem.addEventListener("click", () => {
     p2Elem.innerHTML = "just touch me baby";
     imgElem.src = "./images/bo.jpg";
