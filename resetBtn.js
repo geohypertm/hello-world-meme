44902441a5507ebf268af9ec55833eba053254b2
AmirBiG <itsamirbig@gmail.com>
2025-01-13 15:18:55 +0330

    commit2(amir)

---
+++ resetBtn.js
@@ -0,0 +1,8 @@
+let btnElem = $.querySelector(".res");
+let imgElem = $.querySelector(".i");
+let p2Elem = $.querySelector(".p1");
+
+btnElem.addEventListener("click", () => {
+    p2Elem.innerHTML = "just touch me baby";
+    imgElem.src = "./images/bo.jpg";
+})
